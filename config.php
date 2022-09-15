556e9f1f1eaf9ae7ded39c5d82d5da6d511dd3e5
PhuocVinhLee <113574145+PhuocVinhLee@users.noreply.github.com>
2022-09-15 16:12:07 +0700

    Add files via upload
    da oke

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
     session_start();
     $conn = mysqli_connect('localhost','root','','nuocuong');
     $charset =  mysqli_set_charset($conn, "UTF8");
-    // da sua chua
+    // da sua chua ffdv
 ?>
